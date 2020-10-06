168b8e3c1de4a5c0cfa7018bcd783b3c004bf7b0
David Honess <david.honess+github@gmail.com>
2020-10-06 11:10:53 +0200

    improve column naming

--- create_sstv_tx_geometries.sql
+++ create_sstv_tx_geometries.sql
@@ -6,8 +6,12 @@ CREATE TABLE public.sstv_tx_geometries
 (
   "ID" bigint NOT NULL DEFAULT nextval('sstv_tx_geometries_seq'::regclass),
   "LOG_ID" bigint NOT NULL,
-  zone_tx_land_coverage geometry NOT NULL,
-  zone_tx_land_full_image_rx_possible geometry NOT NULL,
+  point_start geometry NOT NULL,
+  point_end geometry NOT NULL,
+  line_points_start_end geometry NOT NULL,
+  iss_horizon_buffer_line_points_start_end geometry NOT NULL, -- ISS horizon land coverage for this image
+  iss_horizon_intersection_buffers_points_start_end geometry NOT NULL, -- Zone where full image reception is possible
+  longitude_shifted boolean NOT NULL,
   CONSTRAINT "sstv_tx_geometries_PK_ID" PRIMARY KEY ("ID"),
   CONSTRAINT "sstv_tx_geometries_FK_LOG_ID" FOREIGN KEY ("LOG_ID")
       REFERENCES public.sstv_tx_log ("ID") MATCH SIMPLE
@@ -18,3 +22,6 @@ WITH (
 );
 ALTER TABLE public.sstv_tx_geometries
   OWNER TO pi;
+COMMENT ON COLUMN public.sstv_tx_geometries.iss_horizon_buffer_line_points_start_end IS 'ISS horizon land coverage for this image';
+COMMENT ON COLUMN public.sstv_tx_geometries.iss_horizon_intersection_buffers_points_start_end IS 'Zone where full image reception is possible';
+
--- create_sstv_tx_log.sql
+++ create_sstv_tx_log.sql
@@ -7,9 +7,11 @@ CREATE TABLE public.sstv_tx_log
   "ID" bigint NOT NULL DEFAULT nextval('sstv_tx_log_seq'::regclass),
   qr_data character varying(8) NOT NULL,
   start_time timestamp without time zone NOT NULL,
-  start_location geometry NOT NULL,
-  end_time timestamp without time zone NOT NULL,
-  end_location geometry NOT NULL,
+  start_lat numeric NOT NULL,
+  start_long numeric NOT NULL,
+  stop_time timestamp without time zone NOT NULL,
+  stop_lat numeric NOT NULL,
+  stop_long numeric NOT NULL,
   image_no integer NOT NULL,
   CONSTRAINT "sstv_tx_log_PK_ID" PRIMARY KEY ("ID")
 )
